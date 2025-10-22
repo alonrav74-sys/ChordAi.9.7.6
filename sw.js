e3f9e926db06d4eb8f6fe635ea81a883a3e89d4a
alonrav74-sys <alonrav74@gmail.com>
2025-10-22 14:21:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,15 +3,12 @@ const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon512.png"
+  "./icon516.png",
+  "./icon192.png"
 ];
-
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
 });
-
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });

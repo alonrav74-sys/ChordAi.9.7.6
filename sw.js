a91de7e6c6df548d6b3394fa494a4cd6036ce6c7
alonrav74-sys <alonrav74@gmail.com>
2025-10-22 20:54:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
-const CACHE_NAME = "chordfinder-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon516.png",
-  "./icons/icon192.png"
-];
-
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+  e.waitUntil(
+    caches.open("chord-finder-cache-v1").then(cache => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    })
+  );
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });

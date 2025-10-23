3c5a46a3a68d328718c4390d3ee150a76ad8bdb3
alonrav74-sys <alonrav74@gmail.com>
2025-10-23 15:02:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('chordfinder-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
